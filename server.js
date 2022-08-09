a06699bdba8ace320d8429cf071c226b9b56fec8
Adam Awartani <adamawartani321@gmail.com>
2022-08-08 22:36:31 -0400

    render html from routing

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const path = require('path');
 
 var express = require('express');
 var app = express();
 
 app.use(express.json());
 app.use(express.static('public'))
+
+// set routing for html file
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 // set port number
 const PORT = process.env.PORT || 3000;
